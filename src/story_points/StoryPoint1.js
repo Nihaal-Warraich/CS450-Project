7ddf44f492546f00dda50bf3ac00534a8e303271
Nihaal Warraich <nihaal.warraich@gmail.com>
2025-12-06 21:18:15 -0500

    setting up app framework and documents

---
+++ src/story_points/StoryPoint1.js
@@ -0,0 +1,5 @@
+// src/story_points/StoryPoint1.js
+
+export default function StoryPoint1() {
+  return <div>Chart 1 will go here</div>;
+}
---
+++ src/story_points/StoryPoint2.js
@@ -0,0 +1,5 @@
+// src/story_points/StoryPoint2.js
+
+export default function StoryPoint2() {
+  return <div>Chart 2 will go here</div>;
+}
